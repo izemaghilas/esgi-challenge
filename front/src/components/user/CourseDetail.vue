74086d8b8d7b672afcced97c16e2f2b5b483b52d
izemaghilas <saidtala23@gmail.com>
2023-02-13 23:11:23 +0100

    [front]CH-15 protect routes

--- front/src/components/user/CourseDetail.vue
+++ front/src/components/user/CourseDetail.vue
@@ -1,71 +1,73 @@
 <template>
-    <div v-if="loading" class="loader">
-        <Loader />
-    </div>
-    <v-container v-else>
-        <v-row>
-            <v-col cols="12" sm="4">
-                <v-img :src="data.course.thumbnail" alt="" class="" />
-            </v-col>
-            <v-col cols="12" sm="8">
-                <v-card-title class="title">{{ data.course.title }}</v-card-title>
-                <v-card-subtitle class="description">{{ data.course.description }}</v-card-subtitle>
-                <v-btn @click="handleVideoPlayClick" prepend-icon="mdi-school" class="button"
-                    style="color:white; background-color: #251d5d;">
-                    Commencer le cours
-                </v-btn>
-            </v-col>
-            <v-col class="mt-0">
-                <v-btn prepend-icon="mdi-close-box" @click="handleReportClick" class="button"
-                    style="color:white; background-color: #251d5d;">Signler ce
-                    cours</v-btn>
-            </v-col>
-            <v-dialog v-model="dialogVideo" fullscreen :scrim="false" transition="dialog-bottom-transition">
+    <RequireAuth>
+        <div v-if="loading" class="loader">
+            <Loader />
+        </div>
+        <v-container v-else>
+            <v-row>
+                <v-col cols="12" sm="4">
+                    <v-img :src="data.course.thumbnail" alt="" class="" />
+                </v-col>
+                <v-col cols="12" sm="8">
+                    <v-card-title class="title">{{ data.course.title }}</v-card-title>
+                    <v-card-subtitle class="description">{{ data.course.description }}</v-card-subtitle>
+                    <v-btn @click="handleVideoPlayClick" prepend-icon="mdi-school" class="button"
+                        style="color:white; background-color: #251d5d;">
+                        Commencer le cours
+                    </v-btn>
+                </v-col>
+                <v-col class="mt-0">
+                    <v-btn prepend-icon="mdi-close-box" @click="handleReportClick" class="button"
+                        style="color:white; background-color: #251d5d;">Signler ce
+                        cours</v-btn>
+                </v-col>
+                <v-dialog v-model="dialogVideo" fullscreen :scrim="false" transition="dialog-bottom-transition">
+                    <v-card>
+                        <v-toolbar dark color="#251d5d">
+                            <v-btn icon dark color="#f4a118" @click="dialogVideo = false">
+                                <v-icon>mdi-close</v-icon>
+                            </v-btn>
+                            <v-toolbar-title class="video-title">{{ data.course.title }}</v-toolbar-title>
+                            <v-spacer></v-spacer>
+                        </v-toolbar>
+                        <div class="video-container">
+                            <iframe allowfullscreen :src="data.course.mediaLink"></iframe>
+                        </div>
+                    </v-card>
+                </v-dialog>
+            </v-row>
+            <v-card class="comments">
+                <v-card-title class="title">Commentaires</v-card-title>
+                <v-card-text>
+                    <Comments :courseId="data.course.id" />
+                </v-card-text>
+            </v-card>
+            <v-dialog v-model="dialogReport" max-width="500px">
                 <v-card>
-                    <v-toolbar dark color="#251d5d">
-                        <v-btn icon dark color="#f4a118" @click="dialogVideo = false">
-                            <v-icon>mdi-close</v-icon>
-                        </v-btn>
-                        <v-toolbar-title class="video-title">{{ data.course.title }}</v-toolbar-title>
+                    <v-card-title>
+                        <span class="headline">Signaler le cours : {{ data.course.title }} .</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-textarea v-model="reportInput" label="Description" outlined></v-textarea>
+                    </v-card-text>
+                    <v-card-actions>
                         <v-spacer></v-spacer>
-                    </v-toolbar>
-                    <div class="video-container">
-                        <iframe allowfullscreen :src="data.course.mediaLink"></iframe>
-                    </div>
+                        <v-btn color="blue darken-1" text @click="dialogReport = false">Annuler</v-btn>
+                        <v-btn color="blue darken-1" text @click="postReport">Signaler</v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-dialog>
-        </v-row>
-        <v-card class="comments">
-            <v-card-title class="title">Commentaires</v-card-title>
-            <v-card-text>
-                <Comments :courseId="data.course.id" />
-            </v-card-text>
-        </v-card>
-        <v-dialog v-model="dialogReport" max-width="500px">
-            <v-card>
-                <v-card-title>
-                    <span class="headline">Signaler le cours : {{ data.course.title }} .</span>
-                </v-card-title>
-                <v-card-text>
-                    <v-textarea v-model="reportInput" label="Description" outlined></v-textarea>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="dialogReport = false">Annuler</v-btn>
-                    <v-btn color="blue darken-1" text @click="postReport">Signaler</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-        <v-snackbar v-model="snackBarShow" :timeout="timeout">
-            {{ snackBarText }}
+            <v-snackbar v-model="snackBarShow" :timeout="timeout">
+                {{ snackBarText }}
 
-            <template v-slot:actions>
-                <v-btn color="blue" variant="text" @click="snackBarShow = false">
-                    Fermer
-                </v-btn>
-            </template>
-        </v-snackbar>
-    </v-container>
+                <template v-slot:actions>
+                    <v-btn color="blue" variant="text" @click="snackBarShow = false">
+                        Fermer
+                    </v-btn>
+                </template>
+            </v-snackbar>
+        </v-container>
+    </RequireAuth>
 </template>
 
 <script setup>
@@ -75,6 +77,7 @@ import { useRoute } from 'vue-router'
 import Loader from '../Loader.vue';
 import Comments from './Comments.vue';
 import useUser from '../../hooks/useUser';
+import RequireAuth from '../RequireAuth.vue';
 
 const route = useRoute()
 const api = useApi()
@@ -98,7 +101,7 @@ onMounted(async () => {
         const response = await api.getCourseById(route.params.id)
         data.course = response
     } catch (error) {
-        console.log(error)
+        console.error(error)
     } finally {
         loading.value = false
     }
@@ -124,7 +127,7 @@ const postReport = async () => {
         snackBarText.value = "Le cours a été signalé"
         snackBarShow.value = true
     } catch (error) {
-        console.log(error)
+        console.error(error)
     } finally {
         loading.value = false
         dialogReport.value = false
