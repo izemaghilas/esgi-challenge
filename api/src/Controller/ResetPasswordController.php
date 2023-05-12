c4fbca647dc22093d82e4f71c6d2c46ab339e950
izemaghilas <saidtala23@gmail.com>
2023-05-12 16:34:14 +0200

    CH-67 [back] reset password
    create mail template
    send reset password mail

--- api/src/Controller/ResetPasswordController.php
+++ api/src/Controller/ResetPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Service\VerifyEmailService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,10 +11,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 #[AsController]
 class ResetPasswordController extends AbstractController
@@ -23,7 +26,8 @@ class ResetPasswordController extends AbstractController
         private EntityManagerInterface $entityManager,
         private readonly UserRepository $userRepository,
         private readonly UserPasswordHasherInterface $userPasswordHasher,
-        private readonly ValidatorInterface $validator
+        private readonly ValidatorInterface $validator,
+        private readonly VerifyEmailService $verifyEmailService
     ) {
     }
 
@@ -33,18 +37,30 @@ class ResetPasswordController extends AbstractController
         $body = $request->toArray();
 
         if (
-            null === $userId ||
-            false === Uuid::isValid($userId) ||
             !isset($body['plainPassword']) ||
             $this->validator->validate($body['plainPassword'], new Assert\NotCompromisedPassword())->count()
         ) {
-            throw new BadRequestHttpException();
+            throw new UnprocessableEntityHttpException();
+        }
+
+        if (
+            null === $userId ||
+            false === Uuid::isValid($userId)
+        ) {
+            throw new NotFoundHttpException('');
         }
 
         $user = $this->userRepository->find($userId);
         if (null === $user) {
             throw new NotFoundHttpException('');
         }
+
+        try {
+            $this->verifyEmailService->verify($request->getUri(), $user->getId(), $user->getEmail());
+        } catch (VerifyEmailExceptionInterface $e) {
+            throw new BadRequestHttpException();
+        }
+
         $hashedPassword = $this->userPasswordHasher->hashPassword($user, $body['plainPassword']);
         $user->setPassword($hashedPassword);
         $user->eraseCredentials();
